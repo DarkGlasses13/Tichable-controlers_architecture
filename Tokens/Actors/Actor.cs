9aedf39fbc270a9751c3616a58db9624425af5c4
Matroskin_Cat <groznov3@gmail.com>
2023-09-22 14:02:20 +0300

    Fix

--- Tokens/Actors/Actor.cs
+++ Tokens/Actors/Actor.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace Assets.Package.Tokens.Actors
 {
-    public abstract class Actor : Token, IInstanceLoader
+    public abstract class Actor : Token
     {
         private readonly IInstanceLoader _instanceLoader;
         private GameObject _instance;
@@ -16,16 +16,16 @@ namespace Assets.Package.Tokens.Actors
 
         public bool HasInstance => _instance != null;
 
-        public GameObject GetInstance(string id, Action<GameObject> onLoaded = null)
+        public GameObject GetInstance(Action<GameObject> onLoaded = null)
         {
-            _instance = _instanceLoader.GetInstance(id, onLoaded);
+            _instance = _instanceLoader.GetInstance(Data.ID, onLoaded);
             OnInstanceLoaded(_instance);
             return _instance;
         }
 
-        public GameObject GetInstance(string id, Vector3 position, Quaternion rotation, Transform parent, Action<GameObject> onLoaded = null)
+        public GameObject GetInstance(Vector3 position, Quaternion rotation, Transform parent, Action<GameObject> onLoaded = null)
         {
-            _instance = _instanceLoader.GetInstance(id, position, rotation, parent, onLoaded);
+            _instance = _instanceLoader.GetInstance(Data.ID, position, rotation, parent, onLoaded);
             OnInstanceLoaded(_instance);
             return _instance;
         }
