8713dcb7aea9afa9562dcb4a4dac549c5dcdbfdc
Matroskin_Cat <groznov3@gmail.com>
2023-09-24 19:39:20 +0300

    Update tokens

--- Tokens/Actors/Actor.cs
+++ Tokens/Actors/Actor.cs
@@ -1,48 +1,12 @@
-﻿using Architecture_Base.Asset_Loading;
-using System;
-using UnityEngine;
-
-namespace Assets.Package.Tokens.Actors
+﻿namespace Assets.Package.Tokens.Actors
 {
     public abstract class Actor : Token
     {
-        private readonly IInstanceLoader _instanceLoader;
-        private GameObject _instance;
+        public ActorInstance Instance { get; }
 
-        protected Actor(TokenData data, IInstanceLoader instanceLoader) : base(data)
+        protected Actor(TokenData data, ActorInstance instance) : base(data)
         {
-            _instanceLoader = instanceLoader;
+            Instance = instance;
         }
-
-        public bool HasInstance => _instance != null;
-
-        public GameObject GetInstance(Action<GameObject> onLoaded = null)
-        {
-            _instance = _instanceLoader.GetInstance(Data.ID, onLoaded);
-            OnInstanceLoaded(_instance);
-            return _instance;
-        }
-
-        public GameObject GetInstance(Vector3 position, Quaternion rotation, Transform parent, Action<GameObject> onLoaded = null)
-        {
-            _instance = _instanceLoader.GetInstance(Data.ID, position, rotation, parent, onLoaded);
-            OnInstanceLoaded(_instance);
-            return _instance;
-        }
-
-        protected virtual void OnInstanceLoaded(GameObject instance) 
-        {
-            instance
-                .AddComponent<ActorInstance>()
-                .Construct(Data.ID);
-        }
-
-        public void UnloadInstance()
-        {
-            _instanceLoader.UnloadInstance();
-            OnInstanceUnloaded();
-        }
-
-        protected virtual void OnInstanceUnloaded() { }
     }
 }
