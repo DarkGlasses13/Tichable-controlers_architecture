480b0d56783a4a5d666a7fef3bbd06457a993f06
Matroskin_Cat <groznov3@gmail.com>
2023-09-24 19:35:22 +0300

    Update asset loading

--- Asset Loading/AssetLoader.cs
+++ Asset Loading/AssetLoader.cs
@@ -1,13 +1,15 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using UnityEditor.VersionControl;
 using UnityEngine;
 
 namespace Architecture_Base.Asset_Loading
 {
-    public abstract class AssetLoader<T> : IAssetLoader<T>
+    public abstract class AssetLoader<T> : ISingleAssetLoader<T>
     {
+        protected object _asset;
+
         public abstract object Key { get; }
-        public object Asset { get; protected set; }
 
         public abstract T Load();
         public abstract IList<T> LoadAll();
@@ -19,15 +21,15 @@ namespace Architecture_Base.Asset_Loading
 
         public void Unload()
         {
-            if (Asset != null)
+            if (_asset != null)
             {
-                if (Asset is GameObject gameObjectAsset)
+                if (_asset is GameObject gameObjectAsset)
                 {
                     gameObjectAsset.SetActive(false);
                 }
 
                 ReleaseAsset();
-                Asset = null;
+                _asset = null;
             }
         }
 
--- Asset Loading/ISingleAssetLoader.cs
+++ Asset Loading/ISingleAssetLoader.cs
@@ -1,20 +1,15 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using UnityEngine;
 
 namespace Architecture_Base.Asset_Loading
 {
-    public interface IAssetLoader<T>
+    public interface ISingleAssetLoader<T>
     {
         object Key { get; }
-        object Asset { get; }
         T Load();
-        IList<T> LoadAll();
         T LoadAndInstantiate(Transform parent, bool isActive = true);
         Task<T> LoadAsync();
-        Task<IList<T>> LoadAllAsync();
         Task<T> LoadAndInstantiateAsync(Transform parent, bool isActive = true);
-        Task<IList<T>> LoadAndInstantiateAllAsync(Transform parent, bool isActive = true);
         void Unload();
     }
 }
