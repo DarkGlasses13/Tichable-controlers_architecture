61680bbbe2010de9a960dc0f028a060990ad4963
Matroskin_Cat <groznov3@gmail.com>
2023-09-24 19:55:24 +0300

    Revert "Fix"
    This reverts commit 065d55ea57382c05f0a4061dedd950507783ff53.

--- Asset Loading/AssetLoader.cs
+++ Asset Loading/AssetLoader.cs
@@ -1,10 +1,11 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using UnityEditor.VersionControl;
 using UnityEngine;
 
 namespace Architecture_Base.Asset_Loading
 {
-    public abstract class SingleAssetLoader<T> : ISingleAssetLoader<T>
+    public abstract class AssetLoader<T> : ISingleAssetLoader<T>
     {
         protected object _asset;
 
